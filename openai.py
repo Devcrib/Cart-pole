e1a9318972f18cfda73897e39bb867b933099286
victor-iyiola <javafolabi@gmail.com>
2017-08-23 17:24:42 +0100

    A random game instance

--- openai.py
+++ openai.py
@@ -14,4 +14,19 @@ goal_step = 500
 score_requirement = 50
 initial_games = 10000
 
+def random_game():
+  for episode in range(5):
+    env.reset()
+    for t in range(goal_step):
+      env.render()
+      action = env.action_space.sample()
+      observation, reward, done, info = env.step(action)
+      if done:
+        break
 
+def start():
+  random_game()
+
+
+if __name__ == '__main__':
+  start()
