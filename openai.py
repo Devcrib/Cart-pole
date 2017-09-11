a7013c0c00c01a0ac2690c15504609c0a911de52
victor-iyiola <javafolabi@gmail.com>
2017-09-11 05:54:36 +0100

    Playing random games

--- openai.py
+++ openai.py
@@ -14,21 +14,23 @@ goal_step = 500
 score_requirement = 50
 initial_games = 10000
 
+
 # __random_game
 def random_game():
-  for episode in range(5):
-    env.reset()
-    for t in range(goal_step):
-      env.render()
-      action = env.action_space.sample()
-      observation, reward, done, info = env.step(action)
-      if done:
-        break
+    for episode in range(5):
+        env.reset()
+        for t in range(goal_step):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                break
+
 
 # __start
 def start():
-  random_game()
+    random_game()
 
 
 if __name__ == '__main__':
-  start()
+    start()
